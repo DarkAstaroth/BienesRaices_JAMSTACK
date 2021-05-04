23fa1c5cbfca00536c3c471d685e69dbcfb377d7
Manuel <mninahuanca4@gmail.com>
2021-05-04 16:56:43 -0400

    Creando el proyecto de strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e733a24e9dc0403b4068889ea1de8535'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1866a7f1-7f8a-454b-a636-1656dbb278ab'
+};
